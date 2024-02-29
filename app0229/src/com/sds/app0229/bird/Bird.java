102f24e9069fbf58be1b862f6adc8c51832aa1cb
MinTrue <smw362@naver.com>
2024-02-29 13:58:54 +0900

    java_workspace

---
+++ app0229/src/com/sds/app0229/bird/Bird.java
@@ -0,0 +1,18 @@
+package com. sds.app0229.bird;
+
+//모든 새의 어버이인 그냥 새를 정의해본다.
+public class Bird{
+	public String name="난 그냥 새";
+	public  int age=23;
+	/*
+	public Duck(){
+		super();
+	}
+	*/
+	public void eat(){
+		System.out.println("먹이를 먹어요");
+	}
+	public void fly(){
+		System.out.println("부모 새가 날아요");
+	}
+}
