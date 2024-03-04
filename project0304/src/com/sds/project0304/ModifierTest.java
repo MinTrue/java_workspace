0108b935e25d061a3ed7ca10f5cd44cf7d2cb02d
MinTrue <smw362@naver.com>
2024-03-04 16:53:47 +0900

    java_workspace

---
+++ project0304/src/com/sds/project0304/ModifierTest.java
@@ -0,0 +1,16 @@
+package com.sds.project0304;
+
+/*
+ static : 변수, 메서드등을 클래스 원본에 위치시키는 수식자
+ abstract : 추상스, 추상메서드 선언시 사용
+ final : js const 동일 의미 final로 선언된 변수는 변경이 불가함
+           class에 붙이면 더이상 상속을 허락하지 않겠다
+ */
+public class ModifierTest {
+
+	public static void main(String[] args) {
+		final int x=5; // 이 시짐부터는 변경이 불가함
+		
+		
+	}
+}
